9e36662c30ee6470478288427f6952967815e263
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-01-08 14:35:34 -0500

    some additional examples based on ISINGLASS 2

--- vis/loadframe_example.m
+++ vis/loadframe_example.m
@@ -1,17 +1,21 @@
-addpath ~/Projects/GEMINI/vis;     %these need to be altered based on where the GEMINI repo is cloned...
-addpath ~/Projects/GEMINI/script_utils;
-addpath ~/Projects/GEMINI/setup;
+%% LOAD (ONLY) PRE-FLIGHT EVENT
+UTsec=25496;
+ymd=[2017,3,2];
+direc='~/Downloads/isinglass_clayton1/';     %where you've put the data
+[ne,mlatsrc,mlonsrc,xg,v1,Ti,Te,J1,v2,v3,J2,J3,filename,Phitop,ns,vs1,Ts] = loadframe(direc,ymd,UTsec);
 
 
-% %% PRE-FLIGHT EVENT
-% UTsec=25496;
+% %% LOAD FLIGHT EVENT
+% UTsec=28400;
 % ymd=[2017,3,2];
-% direc='~/Downloads/isinglass_clayton1/';     %where you've put the data
+% direc='~/Downloads/isinglass_clayton3/';     %where you've put the data
 % [ne,mlatsrc,mlonsrc,xg,v1,Ti,Te,J1,v2,v3,J2,J3,filename,Phitop,ns,vs1,Ts] = loadframe(direc,ymd,UTsec);
 
 
-%% FLIGHT EVENT
-UTsec=28400;
-ymd=[2017,3,2];
-direc='~/Downloads/isinglass_clayton3/';     %where you've put the data
-[ne,mlatsrc,mlonsrc,xg,v1,Ti,Te,J1,v2,v3,J2,J3,filename,Phitop,ns,vs1,Ts] = loadframe(direc,ymd,UTsec);
+% %% LOAD AND PLOT THE PRE-FLIGHT EVENT (unfortunately this only plots so you don't get stuff in the workspace)
+% UTsec=25496;
+% ymd=[2017,3,2];
+% direc='~/Downloads/isinglass_clayton1/';     %where you've put the data
+% xg = plotframe(direc,ymd,UTsec);
+
+
