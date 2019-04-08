8d4afd6d18df8e778fe1f4a332e629ed4dcecd74
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-04-08 17:00:11 -0400

    added some utilities to grid in magnetic UEN coords.

---
+++ postprocess/example_magUEN_gridding.m
@@ -0,0 +1,111 @@
+cwd = fileparts(mfilename('fullpath'));
+gemini_root = [cwd,filesep,'../../GEMINI'];
+addpath([gemini_root, filesep, 'script_utils'])
+addpath([gemini_root, filesep, 'vis'])
+
+
+direc='~/Projects/GEMINI/objects/test3d_glow/';
+
+ymd=[2013,02,20];
+UTsec=18060;
+
+[ne,mlatsrc,mlonsrc,xg,v1,Ti,Te,J1,v2,v3,J2,J3,filename,Phitop,ns,vs1,Ts] = loadframe(direc,ymd,UTsec);
+[zUENi,xUENi,yUENi,Tei]=model2magUENcoords(xg,Te);
+[zUENi,xUENi,yUENi,J1i]=model2magUENcoords(xg,J1);
+[zUENi,xUENi,yUENi,J3i]=model2magUENcoords(xg,J3);
+[zUENi,xUENi,yUENi,J2i]=model2magUENcoords(xg,J2);
+[zUENi,xUENi,yUENi,nei]=model2magUENcoords(xg,ne);
+[zUENi,xUENi,yUENi,v3i]=model2magUENcoords(xg,v3);
+[zUENi,xUENi,yUENi,v2i]=model2magUENcoords(xg,v2);
+[zUENi,xUENi,yUENi,v1i]=model2magUENcoords(xg,v1);
+[zUENi,xUENi,yUENi,Tii]=model2magUENcoords(xg,Ti);
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,nei(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122)
+imagesc(yUENi,zUENi,squeeze(nei(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,Tei(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122)
+imagesc(yUENi,zUENi,squeeze(Tei(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,J1i(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122);
+imagesc(yUENi,zUENi,squeeze(J1i(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,J3i(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122);
+imagesc(yUENi,zUENi,squeeze(J3i(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,J2i(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122);
+imagesc(yUENi,zUENi,squeeze(J2i(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,v3i(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122);
+imagesc(yUENi,zUENi,squeeze(v3i(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,v2i(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122);
+imagesc(yUENi,zUENi,squeeze(v2i(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,v1i(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122);
+imagesc(yUENi,zUENi,squeeze(v1i(:,end/2,:)));
+axis xy;
+colorbar;
+
+figure;
+subplot(121)
+imagesc(xUENi,zUENi,Tii(:,:,end/2));
+axis xy;
+colorbar;
+subplot(122);
+imagesc(yUENi,zUENi,squeeze(Tii(:,end/2,:)));
+axis xy;
+colorbar;
