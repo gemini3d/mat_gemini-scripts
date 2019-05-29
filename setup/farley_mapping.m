6a26fc66634a22b38a107113322f97bb81a9b340
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-05-29 11:47:43 -0400

    farley mapping updates

--- setup/farley_mapping.m
+++ setup/farley_mapping.m
@@ -65,12 +65,21 @@ while (x1pot(ix1)<x1max)
 end %while
 
 
-%% TEST FIDELITY OF DERIVATIES ON PROPOSED GRID
-
-
-
 %% DIAGNOSTIC PLOTS
 if (flagplots)
     figure;
     plot(x1pot(2:end)/1e3,diff(x1pot)/1e3,'o');
 end %if
+
+
+%% TEST FIDELITY OF DERIVATIES ON PROPOSED GRID
+lx1pot=numel(x1plot);
+x1trans=x1pot(floor(lx1pot/2));
+Phismooth=1/2+1/2*tanh((xg.x1(3:end-2)-x1trans)/10e3);
+Phidec=interp1(xg.x1(3:end-2),Phismooth,x1pot);
+E1dec=gradient(Phidec,x1pot);
+
+
+%E1dec(1)=(-Phidec(3)+4*Phidec(2)-3*Phidec(1))/(x1pot(3)-x1pot(1));    %2nd order BDF
+
+
