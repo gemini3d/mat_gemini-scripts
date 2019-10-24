c8fdb056f9e455cbb6cba87b317e1bd65f55f119
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-10-24 17:13:44 -0400

    corrected some errors with readparticles.m

--- script_utils/readparticles.m
+++ script_utils/readparticles.m
@@ -1,32 +1,36 @@
 function [mlon,mlat,simdate,Qit,E0it]=readparticles(outdir)
 
-% %SET THE PATHS
+% %% SET THE PATHS
 % cwd = fileparts(mfilename('fullpath'));
 % gemini_root = [cwd, filesep, '../../../GEMINI'];
 % addpath([gemini_root, filesep, 'script_utils']);
+% 
+% 
+% %% READ IN THE CONFIG FILE FOR A PARTICULAR RUN
 
 
-%GRAB THE SIZE AND GRID FOR THE INPUT FILES (COULD BE DIFFERENT FROM SIM
+
+%% GRAB THE SIZE AND GRID FOR THE INPUT FILES (COULD BE DIFFERENT FROM SIM
 %GRID)
 filename=[outdir,'simsize.dat'];
-fid=fopen(filename,'w');
+fid=fopen(filename,'r');
 llon=fread(fid,1,'integer*4');
 llat=fread(fid,1,'integer*4');
 fclose(fid);
 
 filename=[outdir,'simgrid.dat'];
-fid=fopen(filename,'w');
+fid=fopen(filename,'r');
 mlon=fread(fid,llon,'real*8');
 mlat=fread(fid,llat,'real*8');
 fclose(fid);
 
 
-%PRODUCE A LIST OF FILES IN THE DIRECTORY
+%% PRODUCE A LIST OF FILES IN THE DIRECTORY
 files=dir(outdir);
-filestrim=[];
 ifile=1;
 for it=1:numel(files)
-   if(~files(it).isdir)
+   if(~files(it).isdir & ~strcmp(files(it).name,'simsize.dat') & ~strcmp(files(it).name,'simgrid.dat') & ...
+           ~strcmp(files(it).name,'particles.mat'))
      filestrim(ifile)=files(it);
      ifile=ifile+1;
    end
@@ -36,21 +40,17 @@ lt=numel(filestrim);
 
 simdate=zeros(lt,6);
 for it=1:lt
-    %UTsec=expdate(it,4)*3600+expdate(it,5)*60+expdate(it,6);
-    %ymd=expdate(it,1:3);
-    %filename=datelab(ymd,UTsec);
     filename=filestrim(it).name;
-    %filename=[outdir,filename,'.dat']
-    fid=fopen(filename,'w');
+    fid=fopen([outdir,filesep,filename],'r');
     datatmp=fread(fid,llon*llat,'real*8');
-    Qit(:,:,it)=datatmp;
+    Qit(:,:,it)=reshape(datatmp,[llon,llat]);
     datatmp=fread(fid,llon*llat,'real*8');
-    E0it(:,:,it)=datatmp;
+    E0it(:,:,it)=reshape(datatmp,[llon,llat]);
     fclose(fid);
     
     ymd=[str2num(filestrim(it).name(1:4)),str2num(filestrim(it).name(5:6)), ...
         str2num(filestrim(it).name(7:8))];
-    UTsec=str2num(filestrim(it).name(9:20));
+    UTsec=str2num(filestrim(it).name(10:21));
     simdate(it,:)=[ymd,UTsec/3600,0,0];
 end
 
