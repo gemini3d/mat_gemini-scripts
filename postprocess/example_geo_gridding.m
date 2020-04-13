4abb9a108a6f353f1d7a3d6f82b07b339c10f239
Matthew Zettergren <mattzett@gmail.com>
2020-04-13 11:28:44 -0400

    udpated gridding code

--- postprocess/example_geo_gridding.m
+++ postprocess/example_geo_gridding.m
@@ -1,26 +1,29 @@
-cwd = fileparts(mfilename('fullpath'));
-gemini_root = [cwd,filesep,'../../GEMINI'];
-addpath([gemini_root, filesep, 'script_utils'])
-addpath([gemini_root, filesep, 'vis'])
+% cwd = fileparts(mfilename('fullpath'));
+% gemini_root = [cwd,filesep,'../../GEMINI'];
+% addpath([gemini_root, filesep, 'script_utils'])
+% addpath([gemini_root, filesep, 'vis'])
+addpath ../script_utils/;
 
-
-direc='~/Projects/GEMINI/objects/test3d_glow/';
+%direc='~/Projects/GEMINI/objects/test3d_glow/';
 %direc='~/simulations/zenodo3d/';
 %direc='~/simulations/junktest3d/';
+direc='~/simulations/EIA_eq/';
 
-ymd=[2013,02,20];
-UTsec=18060;
-
-[ne,mlatsrc,mlonsrc,xg,v1,Ti,Te,J1,v2,v3,J2,J3,filename,Phitop,ns,vs1,Ts] = loadframe(direc,ymd,UTsec);
-[alti,gloni,glati,nei]=model2geocoords(xg,ne);
-[alti,gloni,glati,Tei]=model2geocoords(xg,Te);
-[alti,gloni,glati,J1i]=model2geocoords(xg,J1);
-[alti,gloni,glati,J2i]=model2geocoords(xg,J2);
-[alti,gloni,glati,J3i]=model2geocoords(xg,J3);
-[alti,gloni,glati,v3i]=model2geocoords(xg,v3);
-[alti,gloni,glati,v2i]=model2geocoords(xg,v2);
-[alti,gloni,glati,v1i]=model2geocoords(xg,v1);
-[alti,gloni,glati,Tii]=model2geocoords(xg,Ti);
+ymd=[2011,03,10];
+UTsec=65783;
+file_format='raw';
+realbits=64;
+xg=readgrid(direc,file_format,realbits);
+simdat=loadframe(direc,ymd,UTsec);
+[alti,gloni,glati,nei]=model2geocoords(xg,simdat.ne);
+[alti,gloni,glati,Tei]=model2geocoords(xg,simdat.Te);
+[alti,gloni,glati,J1i]=model2geocoords(xg,simdat.J1);
+[alti,gloni,glati,J2i]=model2geocoords(xg,simdat.J2);
+[alti,gloni,glati,J3i]=model2geocoords(xg,simdat.J3);
+[alti,gloni,glati,v3i]=model2geocoords(xg,simdat.v3);
+[alti,gloni,glati,v2i]=model2geocoords(xg,simdat.v2);
+[alti,gloni,glati,v1i]=model2geocoords(xg,simdat.v1);
+[alti,gloni,glati,Tii]=model2geocoords(xg,simdat.Ti);
 
 
 figure;
@@ -110,5 +113,39 @@ axis xy;
 colorbar;
 subplot(122);
 imagesc(glati,alti,squeeze(Tii(:,end/2,:)));
+axis xy;
+colorbar;
+
+
+%% lat/lon slices
+ialt=min(find(alti>300e3));
+
+figure;
+imagesc(gloni,glati,squeeze(nei(ialt,:,:))');
+axis xy;
+colorbar;
+
+figure;
+imagesc(gloni,glati,squeeze(Tei(ialt,:,:))');
+axis xy;
+colorbar;
+
+figure;
+imagesc(gloni,glati,squeeze(v1i(ialt,:,:))');
+axis xy;
+colorbar;
+
+figure;
+imagesc(gloni,glati,squeeze(v2i(ialt,:,:))');
+axis xy;
+colorbar;
+
+figure;
+imagesc(gloni,glati,squeeze(v3i(ialt,:,:))');
+axis xy;
+colorbar;
+
+figure;
+imagesc(gloni,glati,squeeze(Tii(ialt,:,:))');
 axis xy;
 colorbar;
--- script_utils/geog2dipole.m
+++ script_utils/geog2dipole.m
@@ -1,6 +1,6 @@
 function [q,p,phi]=geog2dipole(alt,glon,glat)
 
-addpath ../../GEMINI/script_utils;    %needs to be made more general
+%addpath ../../GEMINI/script_utils;    %needs to be made more general
 
 [theta,phi]=geog2geomag(glat,glon);
 mlat=90-theta*180/pi;
