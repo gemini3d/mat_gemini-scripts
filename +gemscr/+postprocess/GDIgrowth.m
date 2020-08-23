8775605b359e3fd5625294c8b980b20bbf274048
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-22 22:24:37 -0400

    new loadframe api

--- +gemscr/+postprocess/GDIgrowth.m
+++ +gemscr/+postprocess/GDIgrowth.m
@@ -1,140 +1,143 @@
+function GDIgrowth(direc)
+cwd = fileparts(mfilename('fullpath'));
+run(fullfile(cwd, '../../setup.m'))
+
+narginchk(1,1)
+
+drift_velocity = 0.5e3; % [meters / sec]
+gradient_scale = 10e3; % [meters]
 %% Load a GDI simulation
-%direc='~/simulations/GDI_periodic_lowres_nocap/';
-direc='~/simulations/GDI_ionospheric_noinertia_paper/';
-xg= gemini3d.readgrid(fullfile(direc, 'inputs'));
+xg = gemini3d.readgrid(direc);
 cfg = gemini3d.read_config(direc);
-
-
 %% Pick a reference point to extract a line of density
 x2=xg.x2(3:end-2);
 x3=xg.x3(3:end-2);
-lx3=numel(x3);
-ix1=min(find(xg.x1>300e3));    %F-region location in terms of grid index
+ix1=find(xg.x1>300e3, 1);    %F-region location in terms of grid index
 x2ref=-85e3;
-
-
 %% Loop over data and pull in the density for main part of gradient
-neline=[];
-t=0;
-it=1;
-UTsec=UTsec0;
-ymd=ymd0;
-while (t<=tdur)
-    simdatenow=[ymd,0,0,UTsec]
-    simdate(it,:)=simdatenow;
-    data= gemini3d.vis.loadframe( gemini3d.get_frame_filename(direc,ymd,UTsec));
-    x2now=x2ref+t*0.5e3;    %moving at 0.5 kms/
-    ix2=min(find(x2>x2now));
-    neline(it,:)=squeeze(data.ne(ix1,ix2,:));
+Nt = numel(cfg.times);
+neline = nan(Nt, numel(x3));
 
-    t=t+dtout;
-    it=it+1;
-    [ymd,UTsec]= gemini3d.dateinc(dtout,ymd,UTsec);
-end %while
+for i = 1:length(cfg.times)
+  data = gemini3d.vis.loadframe(direc, cfg.times(i), {'ne'});
+  t_elapsed = seconds(cfg.times(i) - cfg.times(1));
+  x2now = x2ref + t_elapsed * drift_velocity;    %moving at 0.5 kms/
+  ix2 = find(x2 > x2now, 1);
 
-
+  neline(i,:) = data.ne(ix1,ix2,:);
+end
 %% Compute amplitude of fluctuations (BG subtract)
-lt=size(neline,1);
-dneline=zeros(size(neline));
-meanne=zeros(lt,1);
-for it=1:lt
-    meanne(it)=mean(neline(it,:));
-    dneline(it,:)=neline(it,:)-meanne(it);
-end %for
-
-
+dneline = zeros(size(neline));
+meanne = zeros(Nt,1);
+for i=1:Nt
+  meanne(i) = mean(neline(i,:));
+  dneline(i,:)= neline(i,:) - meanne(i);
+end
 %% Fluctuation average and relative change
 % The GDI growth rate
-nepwr=std(dneline,0,2);   %compute a standard deviation along the x2-direction on the grid (tangent to gradient)
-nerelpwr=nepwr./meanne;
-
+nepwr = std(dneline, 0, 2);   %compute a standard deviation along the x2-direction on the grid (tangent to gradient)
+nerelpwr = nepwr ./ meanne;
 
 %% Evaluate time constant empirically from the simulation output
 tconsts=log(nepwr);       % time elapsed measured in growth times
 dtconsts=diff(tconsts);   % difference in time constants between outputs
-itslinear=find(nerelpwr<0.1);
+itslinear = find(nerelpwr<0.1);
 itmin=5;                  % first few frames involve some settling, remove these
 avgdtconst=mean(dtconsts(itmin:max(itslinear)));   % average time constants elapsed per output, only use times after itmin output to allow settling from initial condition
-growthtime=dtout/avgdtconst;
-
-
+growthtime = cfg.dtout/avgdtconst;
 %% Growth rate from linear estimate Im{omega} = E/(B*ell)
-t=datenum(simdate);
-t0=t(itmin);
-gamma=0.5e3/10e3;     % 0.5 km/s drift, gradient scale ~ 10km, hardcoded specific user defined choices here... OR FIXME:  read in config.nml file and figure it out...
-lineargrowthtime=1/gamma;
-linear_nerelpwr=nerelpwr(itmin)*exp(gamma*(t-t0)*86400);
+gamma = drift_velocity / gradient_scale;
+
+fig1 = make_figure1(cfg.times, itmin, gamma, growthtime, nerelpwr);
+
+fig2 = make_figure2(xg, x3, dneline);
+
+fig3 = make_figure3(cfg.times, itmin, nepwr, gamma, growthtime);
+
+% gemini3d.vis.export_graphics(fig3, fullfile(direc, 'plots/growth_compare.eps'))
+
+end % function
 
 
+function fig = make_figure1(times, it_min, gamma, growth_time, ne_rel_pwr)
+% 0.5 km/s drift, gradient scale ~ 10km, hardcoded specific user defined choices here...
+% FIXME:  read in config.nml file and figure it out...
+lineargrowthtime = 1/gamma;
+linear_nerelpwr = ne_rel_pwr(it_min) * exp(gamma*seconds(times - times(it_min)));
 %% Do some basic plot containing this info (avg'd over space)
-figure;
-plot(t,100*nerelpwr);    % growth from simulation
-ax=axis;
+fig = figure;
+ax = axes(fig);
 hold on;
-plot(t,100*linear_nerelpwr);    %pure linear growth
+plot(times,100*ne_rel_pwr);    % growth from simulation
+plot(times,100*linear_nerelpwr);    %pure linear growth
 hold off;
-axis(ax);
 datetick;
-legend({'simulation','linear growth'})
-xlabel('UT');
-ylabel('% variation from background (avg.)');
-title(sprintf('Theoretical \\tau:  %d; model \\tau:  %d',lineargrowthtime,growthtime));
+legend(ax, {'simulation','linear growth'})
+xlabel(ax, 'UT');
+ylabel(ax, '% variation from background (avg.)');
+title(ax, sprintf('Theoretical \\tau:  %d; model \\tau:  %d',lineargrowthtime,growth_time));
 
+end % function
+
+
+function fig = make_figure2(xg, x3, dneline)
 
 %% Break down the growth according to wavenumber
-dx=xg.x3(2)-xg.x3(1);            %grid spacing
-M=2*numel(x3)-1;                     %number of spatial samples in acf
-k=2*pi*[-(M-1)/2:(M-1)/2]/M/dx;  %wavenumber axis for fft
-Snn=zeros(lt,numel(k));
-for it=1:lt
-    acfne=xcorr(dneline(it,:));           %autocorrelation function (spatial) of density variations at this time
-    Snn(it,:)=fftshift(fft(acfne));         %power spectral density, no window
-end %for
-Snnmag=abs(Snn);
-Snnmag(:,lx3)=NaN;
-
+dx = xg.x3(2)-xg.x3(1);            %grid spacing
+M = 2*numel(x3)-1;                     %number of spatial samples in acf
+k = 2*pi* (-(M-1)/2:(M-1)/2) / M /dx;  %wavenumber axis for fft
+Nt = size(dneline, 1);
+Snn = zeros(Nt, numel(k));
+for i = 1:Nt
+  acfne = xcorr(dneline(i,:));           %autocorrelation function (spatial) of density variations at this time
+  Snn(i,:) = fftshift(fft(acfne));         %power spectral density, no window
+end
+Snn_mag = abs(Snn);
+Snn_mag(:, numel(x3))=NaN;
 
 %% Plot wavenumber dependent growth
-figure;
+fig = figure;
 
-its=2*[2,4,6,8,10,12];
-subplot(121);
-plot(x3,dneline(its,:));
-xlabel('horizontal distance (m)');
-ylabel('\Delta n_e');
-for it=1:numel(its)
-  leg{it}=[num2str(its(it)*10),' s'];
-end %for
-legend(leg);
+its = 2*[2,4,6,8,10,12];
+t = tiledlayout(fig, 1, 2);
+ax = nexttile();
+plot(ax, x3, dneline(its,:));
+xlabel(ax, 'horizontal distance (m)');
+ylabel(ax, '\Delta n_e');
+for i=1:numel(its)
+  leg{i} = sprintf('%d s', its(i)*10);
+end
+legend(ax, leg);
 
-subplot(122);
-semilogy(k,Snnmag(its,:));
-xlabel('wavenumber (1/m)');
-ylabel('\Delta n_e power spectral density');
+ax = nexttile(t);
+semilogy(ax, k, Snn_mag(its,:));
+xlabel(ax, 'wavenumber (1/m)');
+ylabel(ax, '\Delta n_e power spectral density');
+
+end % function
 
 
+function fig = make_figure3(times, it_min, ne_pwr, gamma, growth_time)
 %% Log plot absolute growth of irregularities
-hf3=figure(3);
+lineargrowthtime = 1/gamma;
+fig = figure();
 FS=22;
+ax = axes(fig);
+ref_val= ne_pwr(it_min);
+linear_neabspwr=ref_val*exp(gamma*seconds(times - times(it_min)));
 
-refval=nepwr(itmin);
-linear_neabspwr=refval*exp(gamma*(t-t0)*86400);
+semilogy(ax, times(it_min:end), ne_pwr(it_min:end),'LineWidth',1.5);
 
-semilogy(t(itmin:end),nepwr(itmin:end),'LineWidth',1.5);
-set(gca,'FontSize',FS);
-datetick;
-xlabel('UT');
-ylabel('Fluctuation amplitude (m^{-3})');
-axis tight;
-ax=axis;
+datetick(ax);
+xlabel(ax, 'UT');
+ylabel(ax, 'Fluctuation amplitude (m^{-3})');
+
 hold on;
-semilogy(t(itmin:end),linear_neabspwr(itmin:end),'o','LineWidth',1.5);
-axis(ax);
+semilogy(ax, times(it_min:end), linear_neabspwr(it_min:end),'o','LineWidth',1.5);
 hold off;
 
-leglinear=sprintf('linear growth; \\tau = %4.2f s',lineargrowthtime);
-legsim=   sprintf('simulation growth; \\tau = %4.2f s',growthtime);
+leglinear=sprintf('linear growth; \\tau = %4.2f s', lineargrowthtime);
+legsim=   sprintf('simulation growth; \\tau = %4.2f s', growth_time);
 legend(legsim,leglinear,'Location','SouthEast');
 
-%print(3,[direc,'/plots/growth_compare.eps'],'-depsc');
-export_graphics(hf3, fullfile(direc, 'plots/growth_compare.eps'))
+end % function
