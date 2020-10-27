8b311d76d319e8cce584ed4e7f3e461414e402ec
Pavel Inchin <paul.inchin@yahoo.com>
2020-10-27 14:31:53 -0400

    Update makedneuframes2D23D.m

--- magic/makedneuframes2D23D.m
+++ magic/makedneuframes2D23D.m
@@ -1,66 +1,36 @@
-indir='~/zettergmdata/simulations.MAGIC/tohoku//'
-%indir='~/zettergmdata/simulations.MAGIC/okx2/old/'
-%indir='~/neutral_sims/chile06052017/'
-%indir='~/neutral_sims/2016mooreOK/'
-%indir='/media/data/chile2015_0.5MSIS/'
-%indir='/media/data/nepal/'
-loc='';
-%simlab='chile'
-%simlab='nepal'
-simlab='strong'
-%simlab='';
+% This script prepares 3D Cartesian GEMINI neutral inputs from 2D axisymmetric neutral data
 
-%outdir=['~/simulations/mooreOK_neutrals/'];
-%outdir=['~/simulations/chile2015_0.5_neutrals/'];
-%outdir='~/simulations/nepal2015_neutrals/'
-%outdir='~/zettergmdata/simulations/input/mooreOKx2_neutrals/'
-outdir='~/zettergmdata/simulations/input/tohoku3D_neutrals/'
-mkdir(outdir);
+% LOCATION OF OUTPUT DATA
+outdir='./datap/'
+mkdir([outdir]);
 
-%ymd0=[2015,09,16];
-%UTsec0=82473;
-%dtneu=4;
+% Specify date and time of simulation start
+ymd0=[2011,3,11];  % date
+UTsec0=20783;  % Second from day start (UT)
+dtneu=4;  % Sampling of time steps (in seconds)
 
-%ymd0=[2013,05,20];
-%UTsec0=71100;
-%dtneu=6;
-
-%ymd0=[2015,4,24];
-%UTsec0=22285;
-%dtneu=4;
-
-%% TOHOKU EXAMPLE
-ymd0=[2011,3,11];
-UTsec0=20783;
-dtneu=4;
-
-
-%% LOAD THE DATA FROM AN INPUT SIMULATION
+% Thie example expects to load all inputs together in the format (time,rho,z) 
 if ~exist('velx')
     load([indir,'/velx',simlab,loc,'.mat']);
     load([indir,'/velz',simlab,loc,'.mat']);
     load([indir,'/temp',simlab,loc,'.mat']);
-%    load([indir,'/dox2s',simlab,loc,'.mat']);
-%    load([indir,'/dnit2s',simlab,loc,'.mat']);
-%    load([indir,'/doxs',simlab,loc,'.mat']);
     load([indir,'/dox2',simlab,loc,'.mat']);
     load([indir,'/dnit2',simlab,loc,'.mat']);
     load([indir,'/dox',simlab,loc,'.mat']);
 end
-[lt,lrhon,lzn]=size(velx);
 
+[lt,lrhon,lzn]=size(velx); % Get size of the matrix of neutral inputs
 
-%% CREATE A 3D DATA SET OUT OF THE INPUT
-%input grid
-dzn=1e3;
-drhon=1e3;
+%% Create grid of neutral axisymmetric input
+dzn=1e3; % altitude grid size
+drhon=1e3; % radial distance grid size
 zn=0:dzn:(lzn-1)*dzn;
 rhon=0:drhon:(lrhon-1)*drhon;
 
-%output grid
-lx=600;
-ly=600;
-lz=375;
+% Create grid of 3D neutral input
+lx=600; % zonal direction
+ly=600; % meridional direction
+lz=375; % altitude
 x=linspace(-1*max(rhon),max(rhon),lx);    %interpreted as eastward distance
 y=linspace(-1*max(rhon),max(rhon),ly);    %northward distance
 z=linspace(min(zn),max(zn),lz);           %altitude
@@ -68,10 +38,7 @@ z=linspace(min(zn),max(zn),lz);           %altitude
 RHO=sqrt(X.^2+Y.^2);                      %ground distance from epicenter
 PHI=atan2(Y,X);                           %angle from east
 
-
-
-%% CREATE A FILE WITH THE NEUTRAL SIMULATION SIZE
-%system(['rm -rf ',outdir,'/*.dat'])
+% Create a binary file that contain information on neutral input grid size
 filename=[outdir,'simsize.dat']
 fid=fopen(filename,'w');
 fwrite(fid,lx,'integer*4');
@@ -80,7 +47,7 @@ fwrite(fid,lz,'integer*4');
 fclose(fid);
 
 
-%% CYCLE THROUGH INTERPOLATE, ROTATE, AND OUTPUT
+%% Cycle through all epochs, interpolate, rotate and save
 ymd=ymd0;
 UTsec=UTsec0;
 for it=1:lt
@@ -91,9 +58,6 @@ for it=1:lt
     velrho3D(inds)=0;
     velrho3D=reshape(velrho3D,[lz,lx,ly]);
 
-%   @#$%-ing bad math aghhhhhh!!!!!!!!!!!!!!!
-%    velx3D=-1*velrho3D.*sin(PHI);
-%    vely3D=velrho3D.*cos(PHI);
     velx3D=velrho3D.*cos(PHI);
     vely3D=velrho3D.*sin(PHI);
 
