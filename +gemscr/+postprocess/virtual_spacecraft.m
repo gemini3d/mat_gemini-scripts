5894ed72d5b3640b2fddb4e9a9936644136b68c0
Matthew Zettergren <mattzett@gmail.com>
2020-12-07 16:39:51 -0500

    functionalize virtual_spacecraft and commenting

--- +gemscr/+postprocess/virtual_spacecraft.m
+++ +gemscr/+postprocess/virtual_spacecraft.m
@@ -1,26 +1,22 @@
-%% FLIES A VIRTUAL SPACECRAFT THROUGH THE MODEL
-% only works with a cartesian grid for now, and I've found that the code will only work in a reasonable amount of time with plaid interpolations...
+function track=virtual_spacecraft(direc,glonsat,glatsat,altsat,tsat)
+
+% This utility traces a spacecraft through the model domain.  Inputs:
 %
-% not a function, for now.
+%    direc (string) - location of simulation output data, must contain output files,
+%        config files, and grid data
+%    glonsat (#times x #spacecraft) - longitude of spacecraft
+%    glatsat (#times x #spacecraft) - latitude of spacecraft
+%    altsat (#times x #spacecraft) - altitude of spacecraft
+%    tstat (#times) - time basis for the spacecraft, assumed to start at
+%    the beginning of the simulation
 
-
-%imports
-run("../../setup.m");
-
-
-%INPUT DATA
-%direc= '~/simulations/Aether_discrete_50mWm2/';
-%direc= '~/simulations/ARCS_large_intense/';
-direc="~/simulations/arcs_angle_wide_nonuniform/";
-
-
-%%READ IN THE SIMULATION INFORMATION
+% READ IN THE SIMULATION INFORMATION
 cfg = gemini3d.read_config(direc);
 ymd0=cfg.ymd; UTsec0=cfg.UTsec0;
 mloc=[cfg.sourcemlat,cfg.sourcemlon];
 dtout=cfg.dtout; tdur=cfg.tdur;
 
-%CHECK WHETHER WE NEED TO RELOAD THE GRID (WHICH CAN BE TIME CONSUMING)
+% CHECK WHETHER WE NEED TO RELOAD THE GRID (WHICH CAN BE TIME CONSUMING)
 if ~exist('xg','var')
   disp('Loading grid...')
   xg = gemini3d.readgrid(direc);
@@ -34,108 +30,39 @@ if ~exist('xg','var')
   [glatctr,glonctr]= gemini3d.geomag2geog(thetactr,phictr);
 end
 
-
-%% COMPUTE SOURUCE LOCATION IN MCOORDS
-if ~isempty(mloc)
- mlatctr=mloc(1);
- mlonctr=mloc(2);
-else
- mlatctr=[];
- mlonctr=[];
-end
-
-
-%TIMES WHERE WE HAVE MODEL OUTPUT
+% TIMES WHERE WE HAVE MODEL OUTPUT
 times=UTsec0:dtout:UTsec0+tdur;
 lt=numel(times);
 datemod0=datenum([ymd0,UTsec0/3600,0,0]);
 datemod=datemod0:dtout/86400:datemod0+tdur/86400;
 
-%{
-%DEFINE SOME SORT OF SATELLITE ORB.
-%Use a common time bases for all satellites (facilitates processes without having to reread files too much)
-lorb=3000;    %number of times series for satellite orbit
-UTsat=linspace(min(times),max(times),lorb);
-ymdsat=repmat(ymd0,[lorb,1]);
-datevecsat=[ymdsat,UTsat(:)/3600,zeros(lorb,1),zeros(lorb,1)];
-datesat=datenum(datevecsat);
-%}
-
-%{
-%THIS IS A TEST FOR AETHER
-vsp=7e3;                %spacecraft velocity
-toffset=linspace(0,180,12);      %spacecraft separations in seconds
-ltoffset=numel(toffset);
-lsat=ltoffset;
-ysp0=min(xg.x3(:));     %spacecraft reference point is the end of the grid in N-S
-ysp=zeros(lorb,ltoffset);
-for itoffset=1:ltoffset
-  ysp(:,itoffset)=ysp0+vsp*((datesat-datesat(1))*86400-toffset(itoffset));
-end
-xsp=zeros(lorb,ltoffset);
-zsp=400e3*ones(lorb,ltoffset);
-[altsat,glonsat,glatsat]=UEN2geog(zsp,xsp,ysp,thetactr,phictr);
-%}
-
-%load ~/articles/ARCS/orbits_v1.mat;
-%load ~/articles/ARCS/orbits.mat;
-load ~/Documents/proposals/ARCS/scraper/orbits.mat;
+% size on input satellite track
 [lorb,lsat]=size(altsat);
 UTsat=UTsec0+tsat;
 ymdsat=repmat(ymd0,[lorb,1]);
 datevecsat=[ymdsat,UTsat(:)/3600,zeros(lorb,1),zeros(lorb,1)];
 datesat=datenum(datevecsat);
 
-
-%THIS IS A BASIC TWO SATELLITE TEST
-%{
-%Individual orbit alt,lon,lat
-lsat=2;    %number of satellites
-thetasat(:,1)=linspace(min(xg.theta(:)),max(xg.theta(:)),lorb);
-phisat(:,1)=linspace(min(xg.phi(:)),max(xg.phi(:)),lorb);
-altsat(:,1)=linspace(100e3,600e3,lorb);
-thetasat(:,2)=flipud(thetasat(:,1));
-phisat(:,2)=flipud(phisat(:,1));
-altsat(:,2)=flipud(altsat(:,1));
-glatsat=zeros(lorb,lsat);
-glonsat=zeros(lorb,lsat);
-for isat=1:lsat
-  [glatsattmp,glonsattmp]=geomag2geog(thetasat(:,isat),phisat(:,isat));
-  glatsat(:,isat)=glatsattmp(:);
-  glonsat(:,isat)=glonsattmp(:);
-end
-%}
-
 %MAIN LOOP OVER ORBIT SEGMENTS
 datebufprev=datemod0;
 datebufnext=datemod0;
-neprev=[]; nenext=[];
-viprev=[]; vinext=[];
-Tiprev=[]; Tinext=[];
-Teprev=[]; Tenext=[];
-J1prev=[]; J1next=[];
-J2prev=[]; J2next=[];
-J3prev=[]; J3next=[];
-v2prev=[]; v2next=[];
-v3prev=[]; v3next=[];
-nesat=zeros(lorb,lsat);
-visat=zeros(lorb,lsat);
-Tisat=zeros(lorb,lsat);
-Tesat=zeros(lorb,lsat);
-J1sat=zeros(lorb,lsat);
-J2sat=zeros(lorb,lsat);
-J3sat=zeros(lorb,lsat);
-v2sat=zeros(lorb,lsat);
-v3sat=zeros(lorb,lsat);
+track.nesat=zeros(lorb,lsat);
+track.visat=zeros(lorb,lsat);
+track.Tisat=zeros(lorb,lsat);
+track.Tesat=zeros(lorb,lsat);
+track.J1sat=zeros(lorb,lsat);
+track.J2sat=zeros(lorb,lsat);
+track.J3sat=zeros(lorb,lsat);
+track.v2sat=zeros(lorb,lsat);
+track.v3sat=zeros(lorb,lsat);
 
 firstprev=true;
 firstnext=true;
 interptype="linear";
 extraptype="none";
-
 for iorb=1:lorb
   datenow=datesat(iorb);
-
+  
 
   %FIND THE TWO FRAMES THAT BRACKET THIS ORBIT TIME
   datemodnext=datemod0;
@@ -144,10 +71,6 @@ for iorb=1:lorb
     datemodprev=datemodnext;
     datemodnext=datemodnext+dtout/86400;    %matlab datenums are in units of days from 0000
   end
-  %datestr(datemodprev),
-  datestr(datenow),
-  %datestr(datemodnext)
-
 
   if (datemodnext==datemodprev | datemodnext>datemod(end))    %set everything to zero if outside model time domain
     fprintf('Requested time is out of bounds...\n');
@@ -160,13 +83,12 @@ for iorb=1:lorb
     J3prev=neprev; J3next=neprev;
     v2prev=neprev; v2next=neprev;
     v3prev=neprev; v3next=neprev;
-    %datestr(datenow)
     
-    continue;
-  else     %go ahead and read in data and set up the interpolations
+    continue;   % go to next iteration, nothing else to do
+  else     % go ahead and read in data and set up the interpolations
     %DATA BUFFER UPDATES required for time interpolation
     if (datebufprev~=datemodprev | firstprev)    %need to reload the previous output frame data buffers
-      fprintf('Loading previous buffer...\n');
+      fprintf('Loading previous buffer... %s \n',datestr(datenow));
       datevecmodprev=datevec(datemodprev);
       ymd=datevecmodprev(1:3);
       UTsec=datevecmodprev(4)*3600+datevecmodprev(5)*60+datevecmodprev(6);
@@ -191,8 +113,8 @@ for iorb=1:lorb
       
       firstprev=false;
     end
-    if (datebufnext~=datemodnext | firstprev)    %need to reload the next output frame data buffers
-      fprintf('Loading next buffer...\n');
+    if (datebufnext~=datemodnext | firstnext)    %need to reload the next output frame data buffers
+      fprintf('Loading next buffer... %s \n',datestr(datenow));
       datevecmodnext=datevec(datemodnext);
       ymd=datevecmodnext(1:3);
       UTsec=datevecmodnext(4)*3600+datevecmodnext(5)*60+datevecmodnext(6);
@@ -221,6 +143,9 @@ for iorb=1:lorb
 
 
     %INTERPOLATIONS
+    nesattmp=zeros(lsat,1); visattmp=zeros(lsat,1); Tisattmp=zeros(lsat,1); 
+    Tesattmp=zeros(lsat,1); J1sattmp=zeros(lsat,1); J2sattmp=zeros(lsat,1);
+    J3sattmp=zeros(lsat,1); v2sattmp=zeros(lsat,1); v3sattmp=zeros(lsat,1);
     for isat=1:lsat
       [x1sat,x2sat,x3sat]=gemini3d.geog2UEN(altsat(iorb,isat),glonsat(iorb,isat),glatsat(iorb,isat),thetactr,phictr);
       %fprintf('Starting interpolations for satellite:  %d\n',isat);
@@ -258,13 +183,21 @@ for iorb=1:lorb
       v2sattmp(isat)=v2satprev+(v2satnext-v2satprev)/(datemodnext-datemodprev)*(datenow-datemodprev);
       v3sattmp(isat)=v3satprev+(v3satnext-v3satprev)/(datemodnext-datemodprev)*(datenow-datemodprev);
     end
-    nesat(iorb,:)=nesattmp(:)';
-    visat(iorb,:)=visattmp(:)';
-    Tisat(iorb,:)=Tisattmp(:)';
-    Tesat(iorb,:)=Tesattmp(:)';
-    J1sat(iorb,:)=J1sattmp(:)';
-    J2sat(iorb,:)=J2sattmp(:)';
-    J3sat(iorb,:)=J3sattmp(:)';
-    v2sat(iorb,:)=v2sattmp(:)';
-    v3sat(iorb,:)=v3sattmp(:)';
+    track.nesat(iorb,:)=nesattmp(:)';
+    track.visat(iorb,:)=visattmp(:)';
+    track.Tisat(iorb,:)=Tisattmp(:)';
+    track.Tesat(iorb,:)=Tesattmp(:)';
+    track.J1sat(iorb,:)=J1sattmp(:)';
+    track.J2sat(iorb,:)=J2sattmp(:)';
+    track.J3sat(iorb,:)=J3sattmp(:)';
+    track.v2sat(iorb,:)=v2sattmp(:)';
+    track.v3sat(iorb,:)=v3sattmp(:)';
 end
+
+% store input variables in output structure just in case...
+track.glonsat=glonsat;
+track.glatsat=glatsat;
+track.altsat=altsat;
+track.tsat=tsat;
+
+end %function
---
+++ +gemscr/+postprocess/virtual_spacecraft_driver.m
@@ -0,0 +1,65 @@
+%% This is a driver script to show how to use th virtual_spacecraft
+% extraction utility on gemini output.  
+
+% imports
+run("../../setup.m");
+
+% input data
+direc="~/simulations/arcs_angle_wide_nonuniform/";
+
+% Satellite track
+load ~/Documents/proposals/ARCS/scraper/orbits.mat;
+
+% call the tracker
+track=gemscr.postprocess.virtual_spacecraft(direc,glonsat,glatsat,altsat,tsat);
+
+% plot something
+figure(1);
+plot(tsat,track.visat);
+
+
+%% some extra code I don't want to bulldoze yet...
+%{
+%DEFINE SOME SORT OF SATELLITE ORB.
+%Use a common time bases for all satellites (facilitates processes without having to reread files too much)
+lorb=3000;    %number of times series for satellite orbit
+UTsat=linspace(min(times),max(times),lorb);
+ymdsat=repmat(ymd0,[lorb,1]);
+datevecsat=[ymdsat,UTsat(:)/3600,zeros(lorb,1),zeros(lorb,1)];
+datesat=datenum(datevecsat);
+%}
+
+%{
+%THIS IS A TEST FOR AETHER
+vsp=7e3;                %spacecraft velocity
+toffset=linspace(0,180,12);      %spacecraft separations in seconds
+ltoffset=numel(toffset);
+lsat=ltoffset;
+ysp0=min(xg.x3(:));     %spacecraft reference point is the end of the grid in N-S
+ysp=zeros(lorb,ltoffset);
+for itoffset=1:ltoffset
+  ysp(:,itoffset)=ysp0+vsp*((datesat-datesat(1))*86400-toffset(itoffset));
+end
+xsp=zeros(lorb,ltoffset);
+zsp=400e3*ones(lorb,ltoffset);
+[altsat,glonsat,glatsat]=UEN2geog(zsp,xsp,ysp,thetactr,phictr);
+%}
+
+%THIS IS A BASIC TWO SATELLITE TEST
+%{
+%Individual orbit alt,lon,lat
+lsat=2;    %number of satellites
+thetasat(:,1)=linspace(min(xg.theta(:)),max(xg.theta(:)),lorb);
+phisat(:,1)=linspace(min(xg.phi(:)),max(xg.phi(:)),lorb);
+altsat(:,1)=linspace(100e3,600e3,lorb);
+thetasat(:,2)=flipud(thetasat(:,1));
+phisat(:,2)=flipud(phisat(:,1));
+altsat(:,2)=flipud(altsat(:,1));
+glatsat=zeros(lorb,lsat);
+glonsat=zeros(lorb,lsat);
+for isat=1:lsat
+  [glatsattmp,glonsattmp]=geomag2geog(thetasat(:,isat),phisat(:,isat));
+  glatsat(:,isat)=glatsattmp(:);
+  glonsat(:,isat)=glonsattmp(:);
+end
+%}
