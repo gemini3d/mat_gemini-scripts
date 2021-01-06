891c793edd5d476729fc0cdc25d93a84fa4b171d
Matthew Zettergren <mattzett@gmail.com>
2021-01-06 10:14:54 -0500

    functionalize Poynting flux calculations

---
+++ +gemscr/+postprocess/Poynting_calc.m
@@ -0,0 +1,9 @@
+function [E,B,S]=Poynting_calc(direc,time)
+
+% read in a simulation frame (both plasma and magnetic fields are required for
+% the specified frame) and compute the Poynting flux.  Return the electric,
+% magnetic, and poynting flux fields on the grid used by magcalc.
+
+
+
+end function %Poynting_calc
