04e7f2261ef9db19cf7460ff9d2641be3bbf0a1a
Matthew Zettergren <mattzett@gmail.com>
2021-01-06 20:54:21 -0500

    updating Poynting flux comparisons

--- +gemscr/+postprocess/PoyntingConductivity_compare.m
+++ +gemscr/+postprocess/PoyntingConductivity_compare.m
@@ -15,16 +15,59 @@ TOI=datetime([ymd,0,0,UTsec]);
 % Load/recompute auxiliary information
 % call the Poynting flux utility function
 cfg = gemini3d.read.config(direc);    % config file
+disp("Computing Poynting flux...");
 [Ei,B,S,mlon,mlat,datmag,datplasma,xg]=gemscr.postprocess.Poynting_calc(direc,TOI);    % Poynting flux, electric, and magnetic fields
-[sigP,sigH,sig0,SIGP,SIGH,incap,INCAP] = gemscr.postprocess.conductivity_reconstruct(xg,TOI,cfg,datplasma);   % Conductivities on the simulation grid
+disp("Computing conductivities and conductance...");
+[sigP,sigH,sig0,SIGP,SIGH,incap,INCAP]=gemscr.postprocess.conductivity_reconstruct(TOI,datplasma,cfg,xg);   % Conductivities on the simulation grid
 
 % Get the electric fields 
+disp("Computing Ohmic dissipation...")
 [v,E]=gemscr.postprocess.Efield(xg,datplasma.v2,datplasma.v3);   % fields, etc. on the simulation grid
 
 % Compute the Joule dissipation
-E2slice=squeeze(E(end,:,:,2)); E3slice=squeeze(E(end,:,:,3));
-ohmicdissipation=SIGP.*(E2slice.^2+E3slice.^2);
+E2=squeeze(E(:,:,:,2)); E3=squeeze(E(:,:,:,3));
+ohmicdissipation=sigP.*(E2.^2+E3.^2);
 
 % Interpolate the energy dissipation rate onto a magnetic coordinate system
 % for plotting
-[alti,mloni,mlati,ohmici]=model2magcoords(xg,ohmicdissipation,lalt,llon,llat,altlims,mlonlims,mlatlims);
+lalt=256; llon=numel(datmag.mlon); llat=numel(datmag.mlat);
+altlims=[80e3,900e3];
+mlonlims=[min(datmag.mlon),max(datmag.mlon)];
+mlatlims=[min(datmag.mlat),max(datmag.mlat)];
+[alti,mloni,mlati,ohmici]=gemscr.postprocess.model2magcoords(xg,ohmicdissipation,lalt,llon,llat,altlims,mlonlims,mlatlims);
+
+% Compute a field-integrated energy dissipation to compare to Poynting flux
+int_ohmic=zeros(llon,llat);
+for ilon=1:llon
+    for ilat=1:llat
+        int_ohmic(ilon,ilat)=trapz(alti(:),ohmici(:,ilon,ilat),1);
+    end %for
+end %for
+
+% Make a comparison plot
+figure;
+
+subplot(121);
+imagesc(mlon,mlat,squeeze(S(1,:,:,1))*1e3);
+axis xy;
+xlabel("mag. lon. (deg.)");
+ylabel("mag. lat. (deg.)");
+c=colorbar;
+cax=caxis;
+caxval=max(abs(cax));
+caxis([-caxval,caxval]);
+ylabel(c,"Poynting Flux (mW/m^2)");
+title("S_1");
+
+subplot(122);
+int_ohmic=int_ohmic';
+imagesc(mloni,mlati,int_ohmic*1e3);
+axis xy;
+xlabel("mag. lon. (deg.)");
+ylabel("mag. lat. (deg.)");
+c=colorbar;
+cax=caxis;
+caxval=max(abs(cax));
+caxis([-caxval,caxval]);
+ylabel(c,"Energy Dissipation (mW/m^2)");
+title("\Sigma_P E_\perp^2");
