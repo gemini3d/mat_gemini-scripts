a06fddc99cd159f7bda5425822b45968fc064924
Pavel Inchin <paul.inchin@yahoo.com>
2021-02-17 12:11:43 -0500

    Update and rename makedneuframes3Dh5.m to makedneuframes3D-h5.m

---
+++ magic/makedneuframes3D-h5.m
@@ -0,0 +1,66 @@
+% This scripts prepares 3D Cartesian neutral inputs to GEMINI saving them into HDF5 file format
+
+close all
+clc
+
+% Location of output data
+outdir='./datap/';
+
+%% Specify initial time and timestep of data
+ymd0=[2011,3,11];
+UTsec0=20700.0;
+dtneu=10; % timestep
+
+% Specify input data
+lt=1440; % number of time steps
+lx1=880; % zonal direciton
+lx2=1098; % meridional direction
+lx3=500; % vertical direction
+
+% Specify simsize
+filename=[outdir,'simsize.h5'];
+disp("write " + filename)
+if isfile(filename), delete(filename), end
+
+hdf5nc.h5save(filename, '/lx1', lx1, "type", "int32")
+hdf5nc.h5save(filename, '/lx2', lx2, "type", "int32")
+hdf5nc.h5save(filename, '/lx3', lx3, "type", "int32")
+
+freal = 'float32';
+
+ymd=ymd0;
+UTsec=UTsec0;
+
+for it=1:lt
+    
+load(strcat('velx',num2str(it-1),'.mat'),'velxfull');
+load(strcat('vely',num2str(it-1),'.mat'),'velyfull');
+load(strcat('velz',num2str(it-1),'.mat'),'velzfull');
+load(strcat('doxs',num2str(it-1),'.mat'),'doxs');
+load(strcat('dox2s',num2str(it-1),'.mat'),'dox2s');
+load(strcat('dnit2s',num2str(it-1),'.mat'),'dnit2s');
+load(strcat('temps',num2str(it-1),'.mat'),'temps');
+
+% GEMINI requires (alt,lon,lat) structure of data
+velxfull=permute(velxfull,[3,2,1]); % zonal direction
+velyfull=permute(velyfull,[3,2,1]); % meridional direction
+velzfull=permute(velzfull,[3,2,1]);
+temps=permute(temps,[3,2,1]);
+dox2s=permute(dox2s,[3,2,1]);
+dnit2s=permute(dnit2s,[3,2,1]);
+doxs=permute(doxs,[3,2,1]);
+  
+filename=gemini3d.datelab(ymd,UTsec);
+filename=[outdir,filename,'.h5'];
+
+hdf5nc.h5save(filename, '/dn0all', doxs, "type",  freal) % O perturbations
+hdf5nc.h5save(filename, '/dnN2all', dnit2s, "type",  freal) % N2 perturbations
+hdf5nc.h5save(filename, '/dnO2all', dox2s, "type",  freal) % O2 perturbations
+hdf5nc.h5save(filename, '/dvnxall', velyfull, "type",  freal) % Zonal fluid velocity (positive east)
+hdf5nc.h5save(filename, '/dvnrhoall', velxfull, "type",  freal) % Meridional fluid velocity (positive north)
+hdf5nc.h5save(filename, '/dvnzall', velzfull, "type",  freal) % Vertical velocity (positive upward)
+hdf5nc.h5save(filename, '/dTnall', temps, "type",  freal) % Temperature perturbations
+
+    [ymd,UTsec]=gemini3d.dateinc(dtneu,ymd,UTsec);
+    
+end
