db0d38a30abe8e41410dbbeb3020520ac46c81b5
SciVision <scivision@users.noreply.github.com>
2026-02-24 14:35:49 -0700

    note that R2024b or newer needed for auto setup

--- buildfile.m
+++ buildfile.m
@@ -2,10 +2,10 @@ function plan = buildfile
 plan = buildplan(localfunctions);
 plan.DefaultTasks = "setup";
 
-if ~isMATLABReleaseOlderThan("R2024a")
-  plan("check") = matlab.buildtool.tasks.CodeIssuesTask(plan.RootFolder, IncludeSubfolders=true, ...
-      WarningThreshold=0, Results="CodeIssues.sarif");
-end
+assert(~isMATLABReleaseOlderThan("R2024b"), "Matlab >= R2024b needed for auto-setup")
+
+plan("check") = matlab.buildtool.tasks.CodeIssuesTask(plan.RootFolder, IncludeSubfolders=true, ...
+    WarningThreshold=0, Results="CodeIssues.sarif");
 
 end
 
@@ -18,6 +18,7 @@ mat_gemini = fullfile(context.Plan.RootFolder, "mat_gemini");
 matbf = fullfile(mat_gemini, "buildfile.m");
 
 if ~isfile(matbf)
+  % Matlab R2025a has only a GUI submodule update - don't want gitpull() though that updates submodules
   ok = system("git -C " + context.Plan.RootFolder + " submodule update --init --recursive");
   assert(ok == 0, "Failed to update MatGemini Git submodule");
 end
